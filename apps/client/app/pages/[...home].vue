7bd86700b27b55ce247d54077f45e3f0afc6ff89
EliasSchaut <elias@schaut.dev>
2026-04-16 04:10:37 +0200

    update date of bday celebration

--- apps/client/app/pages/[...home].vue
+++ apps/client/app/pages/[...home].vue
@@ -1,34 +1,7 @@
 <template>
   <!-- hero -->
   <section class="z-10 px-6 py-20 pb-16 text-center lg:py-32">
-    <div
-      class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-      aria-hidden="true"
-    >
-      <div
-        class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
-        style="
-          clip-path: polygon(
-            74.1% 44.1%,
-            100% 61.6%,
-            97.5% 26.9%,
-            85.5% 0.1%,
-            80.7% 2%,
-            72.5% 32.5%,
-            60.2% 62.4%,
-            52.4% 68.1%,
-            47.5% 58.3%,
-            45.2% 34.5%,
-            27.5% 76.7%,
-            0.1% 64.9%,
-            17.9% 100%,
-            27.6% 76.8%,
-            76.1% 97.7%,
-            74.1% 44.1%
-          );
-        "
-      ></div>
-    </div>
+    <LayoutBackgroundOne />
     <h1
       class="font-display text-second-900 dark:text-second-100 mx-auto max-w-4xl text-5xl font-medium tracking-tight sm:text-7xl"
     >
@@ -68,34 +41,7 @@
         $t('home.hero.join_community')
       }}</ButtonArrow>
     </div>
-    <div
-      class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-      aria-hidden="true"
-    >
-      <div
-        class="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
-        style="
-          clip-path: polygon(
-            74.1% 44.1%,
-            100% 61.6%,
-            97.5% 26.9%,
-            85.5% 0.1%,
-            80.7% 2%,
-            72.5% 32.5%,
-            60.2% 62.4%,
-            52.4% 68.1%,
-            47.5% 58.3%,
-            45.2% 34.5%,
-            27.5% 76.7%,
-            0.1% 64.9%,
-            17.9% 100%,
-            27.6% 76.8%,
-            76.1% 97.7%,
-            74.1% 44.1%
-          );
-        "
-      ></div>
-    </div>
+    <LayoutBackgroundTwo />
   </section>
 
   <!-- features -->
@@ -235,6 +181,12 @@
     <ArticleOverview
       :title="$t('home.blog.title')"
       :articles="[
+        {
+          title: 'Bday 25',
+          desc: $t('home.blog.bday25.desc'),
+          href: 'https://schaut.dev/articles/bday25',
+          date: `${$t('home.blog.date')} 2025`,
+        },
         {
           title: 'Bday 24',
           desc: $t('home.blog.bday24.desc'),
